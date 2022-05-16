1186836a69238f0954eccd0e2d28fef6b0601f6a
beth nduta <bethnduta05@gmail.com>
2022-05-16 18:18:06 +0300

    deplom

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from data import Blogs
 
 app = Flask(__name__)
+
+Blogs = Blogs()
 
 @app.route('/')
 def index():
@@ -9,6 +12,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html',blogs=Blogs)
 
 
 if __name__ == '__main__':
