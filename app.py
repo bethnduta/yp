72408aaaf5452f52e5d9ec6c8aa51fb5446d79d7
beth nduta <bethnduta05@gmail.com>
2022-05-17 06:18:44 +0300

    forms

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template
-
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '59f407f167538988e3a11cacf723e820'
 
 posts = [
     {
@@ -29,6 +31,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def register():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
