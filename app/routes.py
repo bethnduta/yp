da349b0ec92dbafd20a4c9e617ab183b40f6e6fa
beth nduta <bethnduta05@gmail.com>
2022-05-19 08:40:55 +0300

    deployment

--- app/routes.py
+++ app/routes.py
@@ -100,10 +100,7 @@ def new_post():
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form, legend='new Post')
 
-# @app.route("/post/<int:post_id>")
-# def post(post_id):
-#     post = Post.query.get_or_404(post_id)
-#     return render_template('post.html', title=post.title, post=post)
+
 
 @app.route("/post/<int:post_id>/update",  methods=['GET','POST'])
 @login_required
